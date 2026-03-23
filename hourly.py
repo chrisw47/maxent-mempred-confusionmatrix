ecb354cab78b10ec15c55047d6bff13388012f24
chrisw <chrispersonalemail123@gmail.com>
2026-03-23 12:56:02 -0700

    finished plotting new figs

--- hourly.py
+++ hourly.py
@@ -35,16 +35,14 @@ def hour_by_hour(x_list: list, XTYPE: str):
             raise ValueError(
                 'The stimulation type needs to be global or local.')
 
-        corrs = construct_corr_matrix(df)
+        # corrs = construct_corr_matrix(df)
 
-        subsystem, sub_corrs = filter_neurons(corrs, 60, SIZE)
+        # subsystem, sub_corrs = filter_neurons(corrs, 60, SIZE)
 
-        print(f'NEURONS CHOSEN:\t{subsystem}.')
-        print(f'CORRELATIONS:\t{sub_corrs}.\n')
-
-        _, stim, net = select_neuron_subset(df, subsystem)
-
+        # print(f'NEURONS CHOSEN:\t{subsystem}.')
+        # print(f'CORRELATIONS:\t{sub_corrs}.\n')
         hours = np.arange(0.5, 20.5, 1)
+        system_sizes = [2,3,4,5,6]
 
         for hour in hours:
 
@@ -53,44 +51,77 @@ def hour_by_hour(x_list: list, XTYPE: str):
 
             cond = np.where(np.abs(t - hour*36000) <= 18000)
             mask = np.isin(t, cond)
+            subset = set([60])
 
-            sub_stim = stim[mask]
-            sub_net = net[mask, :]
+            for s in system_sizes:
+                neurons_to_try = set(range(1, 61)) - subset
 
-            j = maxent(sub_stim, sub_net, SIZE)
-            ba, pa, _, _ = cm_metrics(sub_stim, sub_net, j)
+                print(f'Neurons to try:\t{neurons_to_try}.')
 
-            print(ba, pa)
+                # list of predictive accuracies to document which neuron adds most beneficially to overall predictive accuracy.
+                metrics = []
 
-            bx.append(ba)
-            px.append(pa)
+                for n in neurons_to_try:
+                    subset.add(n)
 
+                    print(f'Trying subset containing neurons:\t {sorted(subset)}.')
+
+                    _, ts_stim, ts_net = select_neuron_subset(
+                        df, sorted(subset))  # time bins are rows, ts_stim: timeshifted stimulus array, ts_net: timeshifted network array. this selection applies when we choose off of optimal contribution to predictive accuracy.
+                    
+                    sub_stim, sub_net = ts_stim[mask], ts_net[mask, :]
+
+                    j = maxent(sub_stim, sub_net, s)
+
+                    ba, pa, mn, mp = cm_metrics(sub_stim, sub_net, j)
+
+                    metrics += [n, ba, pa, mn, mp]
+                    subset.remove(n)
+
+                    print(
+                        f'List of neuron indices and predictive accuracies:\n\n{metrics}')
+
+                metrics = np.array(metrics).reshape(
+                    (len(neurons_to_try), 5))  # reshapes into an nx5 array
+
+                # Find the neuron that maximizes model predictive accuracy
+                max_idx = np.argmax(metrics[:, 2])
+                best_neuron_idx = int(metrics[max_idx, 0])
+                subset.add(best_neuron_idx)
+
+                ba, pa, mn, mp = metrics[max_idx, 1:]
+
+                if s == 6:
+                    bx.append(ba)
+                    px.append(pa)
+                
         b.append(bx)
         p.append(px)
 
     b, p = np.array(b), np.array(p)
 
-    np.save(f'{XTYPE}/{XTYPE} hh baseline.npy', b)
-    np.save(f'{XTYPE}/{XTYPE} hh model.npy', p)
+    np.save(f'{XTYPE}/{XTYPE} hh baseline revised.npy', b)
+    np.save(f'{XTYPE}/{XTYPE} hh model revised.npy', p)
 
 
 if __name__ == '__main__':
-    # xl = [9, 12, 14, 15, 16, 17, 18, 19]
+    # # xl = [9, 12, 14, 15, 16, 17, 18, 19]
     # xg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    # hour_by_hour(xl, 'local')
+    # # hour_by_hour(xl, 'local')
     # hour_by_hour(xg, 'global')
-    blhh = np.load('local/local hh baseline.npy')  # baseline local hour-hour
-    plhh = np.load('local/local hh model.npy')
-    bghh = np.load('global/global hh baseline.npy')
-    pghh = np.load('global/global hh model.npy')
+    # quit()
+    blhh = np.load('local/local hh baseline revised.npy')  # baseline local hour-hour
+    plhh = np.load('local/local hh model revised.npy')
+    bghh = np.load('global/global hh baseline revised.npy')
+    pghh = np.load('global/global hh model revised.npy')
 
     # print(f'Baseline global:\n\n{bghh}\n\nModel global:\n\n{pghh}')
 
-    pghhm, pghhci = mean_and_ci(pghh, 90)
-    plhhm, plhhci = mean_and_ci(plhh, 90)
+    pghhm, pghhci = mean_and_ci(pghh, 95)
+    plhhm, plhhci = mean_and_ci(plhh, 95)
 
-    bghhm, bghhci = mean_and_ci(bghh, 90)
-    blhhm, blhhci = mean_and_ci(blhh, 90)
+    bghhm, bghhci = mean_and_ci(bghh, 95)
+    blhhm, blhhci = mean_and_ci(blhh, 95)
 
     bhhm = np.mean(np.concatenate((np.atleast_2d(bghhm), np.atleast_2d(blhhm)), axis=0), axis=0)
     bhhci = np.mean(np.concatenate((np.atleast_2d(bghhci), np.atleast_2d(blhhci)), axis=0), axis=0)
@@ -118,5 +149,5 @@ if __name__ == '__main__':
     plt.legend(loc='lower right')
     plt.tight_layout()
 
-    plt.savefig(f'figs/hh {TIMESHIFT * 100}ms.png', dpi=500)
+    plt.savefig(f'figs/hh {TIMESHIFT * 100}ms revised.png', dpi=400)
     plt.show()
